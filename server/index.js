bc3693d3bdaed3659a0f28e3436cd995b28758a5
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-04-21 15:10:09 +0530

    Developed Authentication APIs Registration and login

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
-import { register } from "module";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 //Configuration
 const __filename = fileURLToPath(import.meta.url);
@@ -38,8 +38,11 @@ const storage = multer.diskStorage({
 });
 const uplaod = multer({ storage });
 
-//Routes with files
-app.post("/auth/register",uplaod.single("picture"),register);
+//Routes with file register -> due to use of upload we can't this API to routes folder
+app.post("/auth/register", uplaod.single("picture"), register);
+
+//Routes
+app.use("/auth",authRoutes);
 
 //Mongoose Setup
 const PORT = process.env.PORT || 6001;
@@ -50,4 +53,5 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-  }).catch((error)=>console.log(`${error} did not connect`))
+  })
+  .catch((error) => console.log(`${error} did not connect`));
